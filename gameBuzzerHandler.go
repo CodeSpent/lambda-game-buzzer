3649d129d3d496f95045eaafab0ca2da32cf78b1
CodeSpent <pat@codespent.dev>
2022-05-14 18:39:05 -0400

    feature: initial commit

---
+++ gameBuzzerHandler.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+)
+
+func handleRequest(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	rc := event.RequestContext
+	switch rk := rc.RouteKey; rk {
+	case "$connect":
+		break
+	case "$disconnect":
+		break
+	case "$buzz":
+		break
+	case "$default":
+		break
+	default:
+		log.Fatalf("Unknown RouteKey %v", rk)
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
